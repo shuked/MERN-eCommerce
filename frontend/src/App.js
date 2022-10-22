1cd54f1d87c39a7ee0c5a111cf9be2c2e565de37
Shuked <shukeds@gmail.com>
2022-10-23 02:23:48 +0300

    initial frontend app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Container } from "react-bootstrap"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
 
-const App = () =>  {
-  return (
-    <>
-      <h1>Welcome to eCommerce</h1> 
-    </>
-  );
+
+const App = () => {
+	return (
+		<Router>
+			<Header />
+			<main className="py-3">
+				<Container>
+        <Routes>
+					<Route path='/' element={<HomeScreen />} exact/>
+					<Route path='/products/:id' element={<ProductScreen />}/>
+
+        </Routes>
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	)
 }
 
-export default App;
+export default App
