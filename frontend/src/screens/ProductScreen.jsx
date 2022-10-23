b43b333e69b78c262dbfa6abfb5317d3f097553a
Shuked <shukeds@gmail.com>
2022-10-23 17:39:19 +0300

    initial backend app

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,23 @@
-import React from "react"
+import React, { useEffect, useState} from "react"
 import { Link, useParams } from "react-router-dom"
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap"
 import Rating from "../components/Rating"
-import products from "../products"
+import axios from 'axios'
+
 
 const ProductScreen = () => {
 	const { id } = useParams()
-	const product = products.find((p) => p._id === id)
+	const [product, setProduct] = useState({})
+
+	useEffect(() => {
+		const fetchProduct = async () => {
+			const { data } = await axios.get(`/api/products/${id}`)
+
+			setProduct(data)
+		}
+
+		fetchProduct()
+	},[id])
 
 	return (
 		<>
@@ -58,8 +69,8 @@ const ProductScreen = () => {
 									</Col>
 								</Row>
 							</ListGroup.Item>
-                            <ListGroup.Item>
-								<Button className='btn-block' type='button' disabled={product.countInStock === 0}>add to cart </Button>
+                            <ListGroup.Item xl={12}>
+								<Button className='btn-block w-100' type='button' disabled={product.countInStock === 0}>add to cart </Button>
 							</ListGroup.Item>
 						</ListGroup>
 					</Card>
