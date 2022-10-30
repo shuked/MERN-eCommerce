76b503707f71e046f6bbeab9552879906b178301
Shuked <shukeds@gmail.com>
2022-10-31 01:05:43 +0200

    added user authantication to frontend

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,19 +1,34 @@
 import { createStore, combineReducers, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 import { composeWithDevTools } from "redux-devtools-extension"
-import {productListReducer, productDetailsReducer} from './reducers/productReducers'
-import { cartReducer } from "./reducers/cartReducer";
+import {
+	productListReducer,
+	productDetailsReducer,
+} from "./reducers/productReducers"
+import { cartReducer } from "./reducers/cartReducer"
+import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from "./reducers/userReducer"
 
 const reducer = combineReducers({
-  productList: productListReducer,
-  productDetails: productDetailsReducer,
-	cart: cartReducer
+	productList: productListReducer,
+	productDetails: productDetailsReducer,
+	cart: cartReducer,
+	userLogin: userLoginReducer,
+	userRegister: userRegisterReducer,
+	userDetails: userDetailsReducer,
+	userUpdateProfile: userUpdateProfileReducer
 })
 
-const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+	? JSON.parse(localStorage.getItem("cartItems"))
+	: []
+
+const userInfoFromStorage = localStorage.getItem("userInfo")
+	? JSON.parse(localStorage.getItem("userInfo"))
+	: null
 
 const initialState = {
-	cart : {cartItems: cartItemsFromStorage}
+	cart: { cartItems: cartItemsFromStorage },
+	userLogin: { userInfo: userInfoFromStorage },
 }
 
 const middlewares = [thunk]
